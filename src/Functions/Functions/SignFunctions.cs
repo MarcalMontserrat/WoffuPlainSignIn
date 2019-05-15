8f893488218505cbe31e72e909abf1b2899a4ece
Marçal Montserrat <mmontserrat@plainconcepts.com>
2019-05-15 17:32:10 +0200

    refactor

--- src/Functions/Functions/SignFunctions.cs
+++ src/Functions/Functions/SignFunctions.cs
@@ -18,23 +18,23 @@ namespace Functions.Functions
             _configuration = configuration;
         }
 
-        //[FunctionName("SignIn")]
-        //public async Task SignIn([TimerTrigger("0 0 9 * * MON-FRI")]TimerInfo myTimer, ILogger log)
-        //{
-        //    var authKey = _configuration["AuthToken"];
-        //    var userId = _configuration["UserId"];
+        [FunctionName("SignIn")]
+        public async Task SignIn([TimerTrigger("0 0 9 * * MON-FRI")]TimerInfo myTimer, ILogger log)
+        {
+            var authKey = _configuration["AuthToken"];
+            var userId = _configuration["UserId"];
 
-        //    await SignService.Sign(authKey, Convert.ToInt32(userId));
-        //}
+            await SignService.Sign(authKey, Convert.ToInt32(userId));
+        }
 
-        //[FunctionName("SignOut")]
-        //public async Task SignOut([TimerTrigger("0 30 18 * * MON-FRI")]TimerInfo myTimer, ILogger log)
-        //{
-        //    var authKey = _configuration["AuthToken"];
-        //    var userId = _configuration["UserId"];
+        [FunctionName("SignOut")]
+        public async Task SignOut([TimerTrigger("0 30 18 * * MON-FRI")]TimerInfo myTimer, ILogger log)
+        {
+            var authKey = _configuration["AuthToken"];
+            var userId = _configuration["UserId"];
 
-        //    await SignService.Sign(authKey, Convert.ToInt32(userId));
-        //}
+            await SignService.Sign(authKey, Convert.ToInt32(userId));
+        }
 
         [FunctionName("IsHolidayPost")]
         public async Task IsHolidayPost([HttpTrigger(AuthorizationLevel.Function, "get")]HttpRequest req, ILogger log, ExecutionContext context)
@@ -49,7 +49,7 @@ namespace Functions.Functions
             var authKey = _configuration["AuthToken"];
             var userId = _configuration["UserId"];
 
-            var response = await SignService.IsHoliday(authKey, Convert.ToInt32(userId));
+            var response = await DayService.IsHoliday(authKey, Convert.ToInt32(userId));
             Console.WriteLine(response);
         }
     }
--- src/Functions/Service/SignService.cs
+++ src/Functions/Service/SignService.cs
@@ -23,22 +23,5 @@ namespace Functions.Service
             httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {authToken}");
             await httpClient.SendAsync(message);
         }
-
-        public static async Task<bool> IsHoliday(string authToken, int userId)
-        {
-            var uri = $"https://plainconcepts.woffu.com/api/users/{userId}/requests?pageSize=50&statusId=20";
-
-            var httpClient = new HttpClient();
-            var message = new HttpRequestMessage(HttpMethod.Get, uri);
-            var body = JsonConvert.SerializeObject(new Message(userId));
-
-            message.Content = new StringContent(body, Encoding.UTF8, "application/json");
-            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {authToken}");
-            var response = await httpClient.SendAsync(message);
-
-            var dayInfo = JsonConvert.DeserializeObject<IEnumerable<DayInfo>>(response.Content.ReadAsStringAsync().Result);
-
-            return dayInfo.Any(x => x.StartDate.Date == DateTime.Today.Date);
-        }
     }
 }

9b638a5d5434e1d9d2501e9ef3a554a952b784c2
David Rodríguez Donoso <drodriguez@plainconcepts.com>
2019-05-15 13:00:48 +0200

    SignIn SignOut

--- src/Functions/Functions/SignFunctions.cs
+++ src/Functions/Functions/SignFunctions.cs
@@ -1,11 +1,11 @@
+using Functions.Service;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Services;
 using System;
 using System.Threading.Tasks;
 
-namespace Functions
+namespace Functions.Functions
 {
     public class SignFunctions
     {
@@ -17,7 +17,16 @@ namespace Functions
         }
         
         [FunctionName("SignIn")]
-        public async Task Run([TimerTrigger("0 * * * * *")]TimerInfo myTimer, ILogger log)
+        public async Task SignIn([TimerTrigger("0 0 9 * * MON-FRI")]TimerInfo myTimer, ILogger log)
+        {
+            var authKey = _configuration["AuthToken"];
+            var userId = _configuration["UserId"];
+
+            await SignService.Sign(authKey, Convert.ToInt32(userId));
+        }
+
+        [FunctionName("SignOut")]
+        public async Task SignOut([TimerTrigger("0 30 18 * * MON-FRI")]TimerInfo myTimer, ILogger log)
         {
             var authKey = _configuration["AuthToken"];
             var userId = _configuration["UserId"];

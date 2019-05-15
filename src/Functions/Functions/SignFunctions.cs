01232c392e09cb5222cca1d1f2810f2221cffef6
Marçal Montserrat <mmontserrat@plainconcepts.com>
2019-05-15 17:28:21 +0200

    added isholiday method.

--- src/Functions/Functions/SignFunctions.cs
+++ src/Functions/Functions/SignFunctions.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 
 namespace Functions.Functions
 {
@@ -16,22 +18,39 @@ namespace Functions.Functions
             _configuration = configuration;
         }
 
-        [FunctionName("SignIn")]
-        public async Task SignIn([TimerTrigger("0 0 9 * * MON-FRI")]TimerInfo myTimer, ILogger log)
+        //[FunctionName("SignIn")]
+        //public async Task SignIn([TimerTrigger("0 0 9 * * MON-FRI")]TimerInfo myTimer, ILogger log)
+        //{
+        //    var authKey = _configuration["AuthToken"];
+        //    var userId = _configuration["UserId"];
+
+        //    await SignService.Sign(authKey, Convert.ToInt32(userId));
+        //}
+
+        //[FunctionName("SignOut")]
+        //public async Task SignOut([TimerTrigger("0 30 18 * * MON-FRI")]TimerInfo myTimer, ILogger log)
+        //{
+        //    var authKey = _configuration["AuthToken"];
+        //    var userId = _configuration["UserId"];
+
+        //    await SignService.Sign(authKey, Convert.ToInt32(userId));
+        //}
+
+        [FunctionName("IsHolidayPost")]
+        public async Task IsHolidayPost([HttpTrigger(AuthorizationLevel.Function, "get")]HttpRequest req, ILogger log, ExecutionContext context)
         {
+
+            //var _configuration = new ConfigurationBuilder()
+            //    .SetBasePath(context.FunctionAppDirectory)
+            //    .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+            //    .AddEnvironmentVariables()
+            //    .Build();
+
             var authKey = _configuration["AuthToken"];
             var userId = _configuration["UserId"];
 
-            await SignService.Sign(authKey, Convert.ToInt32(userId));
-        }
-
-        [FunctionName("SignOut")]
-        public async Task SignOut([TimerTrigger("0 30 18 * * MON-FRI")]TimerInfo myTimer, ILogger log)
-        {
-            var authKey = _configuration["AuthToken"];
-            var userId = _configuration["UserId"];
-
-            await SignService.Sign(authKey, Convert.ToInt32(userId));
+            var response = await SignService.IsHoliday(authKey, Convert.ToInt32(userId));
+            Console.WriteLine(response);
         }
     }
 }
